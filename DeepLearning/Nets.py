529f8e68cd91e5831bb6f53c9c0481ceb3d107d3
Theo Vasile <Theo@Theos-MacBook-Air.local>
2019-04-30 12:10:46 -0400

    initialized neural network class

--- DeepLearning/Nets.py
+++ DeepLearning/Nets.py
@@ -26,3 +26,21 @@ class Layer:
     #retrive a specific node from the layer
     def get_node(self, index):
         return self.nodesList[index]
+
+#general neural network structure
+class NeuralNetwork:
+    def __init__(self, layerDepth, learningRate):
+        self.layersList = [] #contains all the layers in the network
+        self.weights = {} #contains connective weights between nodes
+        #iterate through every layer
+        for l in range(0, len(layerDepth)):
+            self.layersList.append(Layer(layerDepth[l])) #add a layer to the layer list
+            ### connect nodes in current layer to nodes in previous layer
+            if l > 0:
+                self.weights[l] = {}
+                #iterate through nodes in current layer
+                for j in range(0, layerDepth[l]):
+                    #iterate through nodes in previous layer
+                    for k in range(0, layerDepth[l-1]):
+                        #make a weight connecting the nodes
+                        self.weights[l][str(k)+str(j)] = np.random.random() * 6 - 3
