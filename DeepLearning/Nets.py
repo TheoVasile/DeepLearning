da615bf243f14ae47be17bf3a89e6bb5f8239a56
Theo Vasile <Theo@Theos-MacBook-Air.local>
2019-04-29 23:46:08 -0400

    added layer class

--- DeepLearning/Nets.py
+++ DeepLearning/Nets.py
@@ -13,3 +13,16 @@ class Node:
         return self.bias
     def set_bias(self, bias):
         self.bias = bias
+
+#layer containing neuron
+class Layer:
+    def __init__(self, depth):
+        self.nodesList = [] #list containing all the nodes in the layer
+        for n in range(0, depth):
+            self.nodesList.append(Node)
+    #get the amount of nodes in the layer
+    def length(self):
+        return len(self.nodesList)
+    #retrive a specific node from the layer
+    def get_node(self, index):
+        return self.nodesList[index]
