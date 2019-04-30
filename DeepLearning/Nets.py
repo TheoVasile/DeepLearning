86cf02bd3e8b72b7a23520903075c28e7cbb2929
Theo Vasile <Theo@Theos-MacBook-Air.local>
2019-04-29 23:40:42 -0400

    made individual node class

--- DeepLearning/Nets.py
+++ DeepLearning/Nets.py
@@ -1,2 +1,15 @@
+import numpy as np
+
+#individual neuron in neural network
 class Node:
-    pass
+    def __init__(self):
+        self.value = 0 #value stored in the neuron
+        self.bias = np.random.random() * 6 - 3 #bias value that offsets node value
+    def get_value(self):
+        return self.value
+    def set_value(self, value):
+        self.value = value
+    def get_bias(self):
+        return self.bias
+    def set_bias(self, bias):
+        self.bias = bias
